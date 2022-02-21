c3e3ceeac10975fda145c155e54641bc951aa8d5
Sabine0 <s.r.schreuder@st.hanze.nl>
2022-02-21 15:50:19 +0100

    Algorithm python examples

---
+++ src/Algorithms/PythonExamples/qleanringdnn_keras.py
@@ -0,0 +1,136 @@
+import numpy as np
+import gym
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.metrics import mean_squared_error
+from matplotlib import pyplot as plt
+from gym import Wrapper
+
+class DQNAgent:
+    def __init__(self, state_size, action_size):
+        self.n_actions = action_size
+
+        self.lr = 0.001
+        self.gamma = 0.99
+        self.exploration_proba = 1.0
+        self.exploration_proba_decay = 0.005
+        self.batch_size = 32
+        
+        # Only store the 2000 last time steps
+        self.memory_buffer= list()
+        self.max_memory_buffer = 2000
+        
+        # We create our model having hidden layers of 24 units (neurones)
+        # The first layer has the same size as a state size
+        # The last layer has the size of actions space
+        self.model = Sequential([
+            Dense(units=24,input_dim=state_size, activation = 'relu'),
+            Dense(units=24,activation = 'relu'),
+            Dense(units=action_size, activation = 'linear')
+        ])
+        self.model.compile(loss="mse",
+                      optimizer = Adam(lr=self.lr))
+        
+    # The agent computes the action to perform given a state 
+    def compute_action(self, current_state):
+        # We sample a variable uniformly over [0,1]
+        # if the variable is less than the exploration probability
+        #     we choose an action randomly
+        # else
+        #     we forward the state through the DNN and choose the action 
+        #     with the highest Q-value.
+        if np.random.uniform(0,1) < self.exploration_proba:
+            return np.random.choice(range(self.n_actions))
+        q_values = self.model.predict(current_state)[0]
+        return np.argmax(q_values)
+
+    # when an episode is finished, we update the exploration probability using 
+    # espilon greedy algorithm
+    def update_exploration_probability(self):
+        self.exploration_proba = self.exploration_proba * np.exp(-self.exploration_proba_decay)
+        print(self.exploration_proba)
+    
+    # At each time step, we store the corresponding experience
+    def store_episode(self,current_state, action, reward, next_state, done):
+        #We use a dictionnary to store them
+        self.memory_buffer.append({
+            "current_state":current_state,
+            "action":action,
+            "reward":reward,
+            "next_state":next_state,
+            "done" :done
+        })
+        # If the size of memory buffer exceeds its maximum, we remove the oldest experience
+        if len(self.memory_buffer) > self.max_memory_buffer:
+            self.memory_buffer.pop(0)
+    
+
+    # At the end of each episode, we train our model
+    def train(self):
+        # We shuffle the memory buffer and select a batch size of experiences
+        np.random.shuffle(self.memory_buffer)
+        batch_sample = self.memory_buffer[0:self.batch_size]
+        
+        # We iterate over the selected experiences
+        for experience in batch_sample:
+            # We compute the Q-values of S_t
+            q_current_state = self.model.predict(experience["current_state"])
+            # We compute the Q-target using Bellman optimality equation
+            q_target = experience["reward"]
+            if not experience["done"]:
+                q_target = q_target + self.gamma*np.max(self.model.predict(experience["next_state"])[0])
+            q_current_state[0][experience["action"]] = q_target
+            # train the model
+            self.model.fit(experience["current_state"], q_current_state, verbose=0)
+
+env = gym.make("CartPole-v1")
+state_size = env.observation_space.shape[0]
+action_size = env.action_space.n
+
+n_episodes = 100
+max_iteration_ep = 50
+
+agent = DQNAgent(state_size=state_size,action_size=action_size)
+total_steps = 0
+
+for e in range(n_episodes):
+    current_state = env.reset()
+    current_state = np.array([current_state])
+    for step in range(max_iteration_ep):
+        total_steps = total_steps + 1
+        action = agent.compute_action(current_state=current_state)
+        next_state, reward, done, _ = env.step(action)
+        next_state = np.array([next_state])
+
+        agent.store_episode(current_state, action, reward, next_state, done)
+
+        if done:
+            agent.update_exploration_probability()
+            break
+        current_state = next_state
+
+    if total_steps >= agent.batch_size:
+        agent.train()
+
+# Watch the agent play (wrapper import doesn't work for me?)
+def make_video():
+    env_to_wrap = gym.make('CartPole-v1')
+    env = Wrapper.Monitor(env_to_wrap, 'videos', force = True)
+    rewards = 0
+    steps = 0
+    done = False
+    state = env.reset()
+    state = np.array([state])
+    while not done:
+        action = agent.compute_action(state)
+        state, reward, done, _ = env.step(action)
+        state = np.array([state])            
+        steps += 1
+        rewards += reward
+    print(rewards)
+    env.close()
+    env_to_wrap.close()
+make_video()
---
+++ src/Algorithms/PythonExamples/qlearningtest.py
@@ -0,0 +1,47 @@
+import numpy as np
+import gym
+
+env = gym.make("FrozenLake-v1")
+n_observations = env.observation_space.n
+n_actions = env.action_space.n
+
+Q_table = np.zeros((n_observations,n_actions))
+# print(Q_table)
+
+n_episodes = 10000
+max_iter_episode = 100
+exploration_proba = 1
+min_exploration_proba = 0.01
+exploration_decreasing_decay = 0.001
+gamma = 0.99
+lr = 0.1
+
+rewards_per_episode = list()
+
+for e in range(n_episodes):
+    current_state = env.reset()
+    done = False
+
+    total_episode_reward = 0
+
+    for i in range(max_iter_episode):
+        if np.random.uniform(0,1) < exploration_proba:
+            action = env.action_space.sample()
+        else:
+            action = np.argmax(Q_table[current_state,:])
+
+        next_state, reward, done, _ = env.step(action)
+
+        Q_table[current_state, action] = (1-lr) * Q_table[current_state, action] + lr*(reward + gamma*max(Q_table[next_state,:]))
+        total_episode_reward = total_episode_reward + reward
+
+        if done:
+            break
+        current_state = next_state
+
+    exploration_proba = max(min_exploration_proba, np.exp(-exploration_decreasing_decay*e))
+    rewards_per_episode.append(total_episode_reward)
+
+print("Mean reward per thousand episodes")
+for i in range(10):
+    print((i+1)*1000, ": mean espiode reward:", np.mean(rewards_per_episode[1000*i:1000*(i+1)]))
