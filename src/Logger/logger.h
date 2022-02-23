b67f3dd00993f7f0167f0c380ee446dabdc0fdc6
douwe <douwe0303@gmail.com>
2022-02-23 16:31:33 +0100

    Changed logger after review

--- src/Logger/logger.h
+++ src/Logger/logger.h
@@ -21,45 +21,38 @@ class Logger {
         };
 
         template<class T>
-        static void log(const T & object) {
-            std::stringstream ss = getSS(object);
-            std::cout << ss.str() << std::endl;
-            writeToFile(ss.str(), activeFile);
+        static void log(const T & object, std::string type="", std::string ascii="", bool timeStamp=false, std::string tabs="") {
+            std::string time;
+            if(!type.empty()) type = "["+type+"]";
+            if(timeStamp) time = Time::currentDateTime()+"\t\t ";
+            if(!ascii.empty()) ascii = "\033["+ascii+"m";
+            std::cout << ascii+type+tabs+"\033[0m"+time+getSS(object).str() << std::endl;
+            writeToFile(type+tabs+time+getSS(object).str(), activeFile);
         };
-
+        
         template<class T>
         static void warning(const T & object) {
-            std::stringstream ss = getSS(object);
-            std::cout << "\033[1;33m[WARNING]\t\033[0m " << Time::currentDateTime() << "\t " << ss.str() << std::endl;
-            writeToFile("[WARNING]\t "+Time::currentDateTime()+": "+ss.str(), activeFile);
+            log(getSS(object).str(), "WARNING", "1;33", true, "\t");
         };
 
         template<class T>
         static void error(const T & object) {
-            std::stringstream ss = getSS(object);
-            std::cout << "\033[1;31m[ERROR]\t\t\033[0m " << Time::currentDateTime() << "\t " << ss.str() << std::endl;
-            writeToFile("[ERROR]\t "+Time::currentDateTime()+": "+ss.str(), activeFile);
+            log(getSS(object).str(), "ERROR", "1;31", true, "\t\t");
         };
 
         template<class T>
         static void success(const T & object) {
-            std::stringstream ss = getSS(object);
-            std::cout << "\033[1;32m[SUCCESS]\t\033[0m " << Time::currentDateTime() << "\t " << ss.str() << std::endl;
-            writeToFile("[SUCCESS]\t "+Time::currentDateTime()+": "+ss.str(), activeFile);
+            log(getSS(object).str(), "SUCCESS", "1;32", true, "\t");
         };
 
         template<class T>
         static void info(const T & object) {
-            std::stringstream ss = getSS(object);
-            std::cout << "\033[1;34m[INFO]\t\t\033[0m " << Time::currentDateTime() << "\t " << ss.str() << std::endl;
-            writeToFile("[SUCCESS]\t "+Time::currentDateTime()+": "+ss.str(), activeFile);
+            log(getSS(object).str(), "INFO", "1;34", true, "\t\t");
         };
 
         template<class T>
         static void debug(const T & object) {
-            std::stringstream ss = getSS(object);
-            std::cout << "\033[1;37m[DEBUG]\t\t\033[0m " << Time::currentDateTime() << "\t " << ss.str() << std::endl;
-            writeToFile("[DEBUG]\t "+Time::currentDateTime()+": "+ss.str(), activeFile);
+            log(getSS(object).str(), "DEBUG", "1;37", true, "\t\t");
         };
     private:
         static std::string getCurrentPath();
