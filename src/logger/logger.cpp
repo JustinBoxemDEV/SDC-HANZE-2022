afa687b3d474b7aa2aace1aa7844db2a6c5e2e7c
douwe <douwe0303@gmail.com>
2022-02-23 13:47:04 +0100

    Added generic type for log function

--- src/logger/logger.cpp
+++ src/logger/logger.cpp
@@ -3,51 +3,20 @@
 #include <filesystem>
 #include <unistd.h>
 
-Logger::Level level;
-std::string activeFile;
+Logger::Level Logger::level = Logger::DEFAULT;
+std::string Logger::activeFile;
 
 Logger::Logger() {
     level = DEFAULT;
 };
 
-void Logger::log(std::string message) {
-    std::string messageLevel = "";
-    switch(level) {
-        case DEBUG: {
-            std::cout << "\033[1;37m[DEBUG]\t\t\033[0m " << Time::currentDateTime() << "\t " + message << std::endl;
-            messageLevel = "[DEBUG]\t\t "+Time::currentDateTime()+": ";
-            break;
-        };
-        case INFO: {
-            std::cout << "\033[1;34m[INFO]\t\t\033[0m " << Time::currentDateTime() << "\t " + message << std::endl;
-            messageLevel = "[INFO]\t\t "+Time::currentDateTime()+": ";
-            break;
-        };
-        case WARNING: {
-            std::cout << "\033[1;33m[WARNING]\t\033[0m " << Time::currentDateTime() << "\t " + message << std::endl;
-            messageLevel = "[WARNING]\t "+Time::currentDateTime()+": ";
-            break;
-        };
-        case ERROR: {
-            std::cout << "\033[1;31m[ERROR]\t\t\033[0m " << Time::currentDateTime() << "\t " + message << std::endl;
-            messageLevel = "[ERROR]\t\t "+Time::currentDateTime()+": ";
-            break;
-        };
-        case SUCCESS: {
-            std::cout << "\033[1;32m[SUCCESS]\t\033[0m " << Time::currentDateTime() << "\t " + message << std::endl;
-            messageLevel = "[SUCCESS]\t "+Time::currentDateTime()+": ";
-            break;
-        };
-        default: {
-            std::cout << message << std::endl;
-            break; 
-        };
-    };
-    if(!activeFile.empty()) {
-        //std::cout << "Writing data to file "+activeFile << std::endl;
-        writeToFile(messageLevel + message, activeFile);
-    };
-};
+Logger::Level Logger::getLevel() {
+    return level;
+}
+
+std::string Logger::getActiveFile() {
+    return activeFile;
+}
 
 void Logger::setLevel(Logger::Level level) {
     switch(level) {
@@ -109,36 +78,6 @@ void Logger::writeToFile(std::string data, std::string fileName) {
     ofile.open(getCurrentPath()+fileName, std::ios_base::app);
     ofile << data+"\n";
     ofile.close();
-};
-
-void Logger::warning(std::string message) {
-    level = WARNING;
-    Logger::log(message);
-    level = DEFAULT;
-};
-
-void Logger::error(std::string message) {
-    level = ERROR;
-    Logger::log(message);
-    level = DEFAULT;
-};
-
-void Logger::info(std::string message) {
-    level = INFO;
-    Logger::log(message);
-    level = DEFAULT;
-};
-
-void Logger::debug(std::string message) {
-    level = DEBUG;
-    Logger::log(message);
-    level = DEFAULT;
-};
-
-void Logger::success(std::string message) {
-    level = SUCCESS;
-    Logger::log(message);
-    level = DEFAULT;
 };
 
 void Logger::setActiveFile(std::string fileName) {
