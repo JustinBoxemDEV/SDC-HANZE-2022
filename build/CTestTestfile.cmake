f418e22bbc5b6c9c8ed6f5308ad928d6c16ed21f
Sabine0 <s.r.schreuder@st.hanze.nl>
2022-02-28 16:06:42 +0100

    More examples

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sab/Documents/Projects/SDC-HANZE-2022
-# Build directory: /home/sab/Documents/Projects/SDC-HANZE-2022/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
