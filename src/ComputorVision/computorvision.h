2310f14e51e44520ec0aeb15888d1a91a1195c62
RobinvanWijk <robinvanwijk@home.nl>
2022-03-11 16:15:57 +0100

    header variables

--- src/ComputorVision/computorvision.h
+++ src/ComputorVision/computorvision.h
@@ -7,13 +7,21 @@ class ComputorVision{
     private:
         cv::Mat frame;
         cv::Point2f dstP[4];
+        float normalisedLaneOffset = 0;
+        float laneOffset = 0;
+        float curveRadiusR = 0;
+        float curveRadiusL = 0;
 
     private:
         cv::Vec2f averageVec2Vector(std::vector<cv::Vec2f> vectors);
         cv::Vec4i GeneratePoints(cv::Mat src, cv::Vec2f average);
     public:
-        void SetFrame(cv::Mat src);
+        float getNormalisedLaneOffset(){ return normalisedLaneOffset; }
+        float getLaneOffset(){ return laneOffset; }
+        float getRightEdgeCurvature(){ return curveRadiusR; }
+        float getLeftEdgeCurvature(){ return curveRadiusL; }
 
+        void SetFrame(cv::Mat src);
         cv::Mat BlurImage(cv::Mat src);
         cv::Mat DetectEdges(cv::Mat src);
         cv::Mat MaskImage(cv::Mat src);
@@ -22,7 +30,6 @@ class ComputorVision{
         cv::Mat PlotLaneLines(cv::Mat src, std::vector<cv::Vec4i> lines);
         std::vector<cv::Point2f> SlidingWindow(cv::Mat image, cv::Rect window);
         std::vector<int> Histogram(cv::Mat src);
-
         cv::Mat CreateBinaryImage(cv::Mat src);
         std::vector<cv::Vec4i> GenerateLines(cv::Mat src);
         void PredictTurn(cv::Mat src, std::vector<cv::Vec4i> edgeLines);
--- src/MediaCapture/mediaCapture.cpp
+++ src/MediaCapture/mediaCapture.cpp
@@ -106,5 +106,16 @@ void MediaCapture::ProcessImage(cv::Mat src)
     cv::Mat maskedImage = cVision.MaskImage(binaryImage);
 
     std::vector<cv::Vec4i> averagedLines = cVision.GenerateLines(maskedImage);
+
+    float laneOffset = cVision.getLaneOffset();
+    float normalisedLaneOffset = cVision.getNormalisedLaneOffset();
+    cv::putText(src, "Center Offset: " + std::to_string(laneOffset), cv::Point(10, 25), 1, 1.2, cv::Scalar(255, 255, 0));
+    cv::putText(src, "Center Offset (N): " + std::to_string(normalisedLaneOffset), cv::Point(10, 50), 1, 1.2, cv::Scalar(255, 255, 0));
+
     cVision.PredictTurn(maskedImage, averagedLines);
+    
+    float curveRadiusR = cVision.getRightEdgeCurvature();
+    float curveRadiusL = cVision.getLeftEdgeCurvature();
+    cv::putText(src, "Curvature left edge: " + std::to_string(curveRadiusL), cv::Point(10, 75), 1, 1.2, cv::Scalar(255, 255, 0));
+    cv::putText(src, "Curvature right edge: " + std::to_string(curveRadiusR), cv::Point(10, 100), 1, 1.2, cv::Scalar(255, 255, 0));
 }
