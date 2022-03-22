dc7758e0f87965c325e3087444a3f2c66784ab21
JustinBoxemDEV <justinboxem@hotmail.com>
2022-03-22 11:37:35 +0100

    CanBUS updated to work on physical kart

--- src/main.cpp
+++ src/main.cpp
@@ -24,81 +24,83 @@ namespace fs = std::filesystem;
 using namespace std;
 
 // [BUILD 1] Without scheduler (For loop send x amount of times)
-// CANStrategy canStrategy;
+CANStrategy canStrategy;
 
-// void recursive() {
-//     int send_amount = 25; // Could make this dynamic from input I guess
-//     int sleep_time = 2;
+void recursive() {
+    int send_amount = 1000; // Could make this dynamic from input I guess
+    int sleep_time = 0.04;
 
-//     std::cout << "(LOOP VERSION) Hi there! ;) What do you want to do? Type throttle, brake, steer or exit" << std::endl;
+    std::cout << "(LOOP VERSION) Hi there! ;) What do you want to do? Type throttle, brake, steer or exit" << std::endl;
 
-//     char input[25];
-//     std::cin.get(input, 25);
-//     std::cin.ignore(256, '\n');
+    char input[25];
+    std::cin.get(input, 25);
+    std::cin.ignore(256, '\n');
 
-//     if (strcmp(input, "throttle") == 0) {
-//         std::cout << "Executing: " << input << std::endl;
+    if (strcmp(input, "throttle") == 0) {
+        std::cout << "Executing: " << input << std::endl;
 
-//         std::cout << "GIVE THROTTLE PERCENTAGE (BETWEEN 0-100) >" << std::endl;
-//         char speed[25];
-//         std::cin.get(speed, 25);
-//         std::cin.ignore(256, '\n');
-//         std::cout << "Speed is: " << speed << std::endl;
+        std::cout << "GIVE THROTTLE PERCENTAGE (BETWEEN 0-100) >" << std::endl;
+        char speed[25];
+        std::cin.get(speed, 25);
+        std::cin.ignore(256, '\n');
+        std::cout << "Speed is: " << speed << std::endl;
 
-//         std::cout << "Sending " << send_amount << " times" << std::endl;
+        std::cout << "Sending " << send_amount << " times" << std::endl;
 
-//         for(int i = 0; i < send_amount; i++) {
-//            canStrategy.forward(std::stoi(speed)); 
-//            sleep(sleep_time);
-//         };
-//         recursive();
-//     } else if (strcmp(input, "brake")==0) {
-//         std::cout << "Executing: " << input << std::endl;
+        for(int i = 0; i < send_amount; i++) {
+           canStrategy.forward(std::stoi(speed)); 
+        //    canStrategy.steer(0.9);
+           sleep(sleep_time);
+        };
+    
+        recursive();
+    } else if (strcmp(input, "brake")==0) {
+        std::cout << "Executing: " << input << std::endl;
 
-//         std::cout << "GIVE BRAKE PERCENTAGE (BETWEEN 0-100) >" << std::endl;
-//         char brakePercentage[25];
-//         std::cin.get(brakePercentage, 25);
-//         std::cin.ignore(256, '\n');
-//         std::cout << "Brake percentage is: " << brakePercentage << std::endl;
+        std::cout << "GIVE BRAKE PERCENTAGE (BETWEEN 0-100) >" << std::endl;
+        char brakePercentage[25];
+        std::cin.get(brakePercentage, 25);
+        std::cin.ignore(256, '\n');
+        std::cout << "Brake percentage is: " << brakePercentage << std::endl;
 
-//         std::cout << "Sending " << send_amount << " times" << std::endl;
+        std::cout << "Sending " << send_amount << " times" << std::endl;
 
-//         for(int i = 0; i < 100; i++) {
-//             canStrategy.brake(std::stoi(brakePercentage));
-//             sleep(sleep_time);
-//         };
-//         recursive();
-//     } else if (strcmp(input, "steer") == 0) {
-//         std::cout << "Executing: " << input << std::endl;
+        for(int i = 0; i < send_amount; i++) {
+            canStrategy.brake(std::stoi(brakePercentage));
+            sleep(sleep_time);
+        };
+        recursive();
+    } else if (strcmp(input, "steer") == 0) {
+        std::cout << "Executing: " << input << std::endl;
 
-//         std::cout << "GIVE STEERING ANGLE (BETWEEN -1.0 AND 1.0) >" << std::endl;
-//         char steeringamount[25];
-//         std::cin.get(steeringamount, 25);  
-//         std::cin.ignore(256, '\n');
-//         std::cout << "Steering amount is: " << steeringamount << std::endl;
+        std::cout << "GIVE STEERING ANGLE (BETWEEN -1.0 AND 1.0) >" << std::endl;
+        char steeringamount[25];
+        std::cin.get(steeringamount, 25);  
+        std::cin.ignore(256, '\n');
+        std::cout << "Steering amount is: " << steeringamount << std::endl;
 
-//         std::cout << "Sending " << send_amount << " times" << std::endl;
+        std::cout << "Sending " << send_amount << " times" << std::endl;
         
-//         for(int i = 0; i < 100; i++) {
-//             canStrategy.steer(std::stof(steeringamount));
-//             sleep(sleep_time);
-//         };
-//         recursive();
-//     }
-//     else if (strcmp(input, "exit") == 0) {
-//         // Close things?
-//         std::cout << "Bye!" << std::endl;
-//     } else {
-//         std::cout << input << " is not a valid command." << std::endl;
-//         memset(input, 0, 25);
-//         recursive();
-//     };
-// };
+        for(int i = 0; i < send_amount; i++) {
+            canStrategy.steer(std::stof(steeringamount));
+            sleep(sleep_time);
+        };
+        recursive();
+    }
+    else if (strcmp(input, "exit") == 0) {
+        // Close things?
+        std::cout << "Bye!" << std::endl;
+    } else {
+        std::cout << input << " is not a valid command." << std::endl;
+        memset(input, 0, 25);
+        recursive();
+    };
+};
 
-// int main() {
-//     std::cout << "test" << std::endl;
-//     recursive();
-// };
+int main() {
+    std::cout << "test" << std::endl;
+    recursive();
+};
 
 
 // [BUILD 2] With scheduler (TODO: refactor. This wont work anymore with the new scheduler)
@@ -126,13 +128,13 @@ using namespace std;
 
 // [BUILD 3] Full build (CAN + Computer vision + PID)
 
-int main(int argc, char** argv) {
-    if (argc == 1) {
-        MediaCapture mediaCapture;
-        mediaCapture.ProcessFeed(0, "");
-        return 0;
-    } 
-}
+// int main(int argc, char** argv) {
+//     if (argc == 1) {
+//         MediaCapture mediaCapture;
+//         mediaCapture.ProcessFeed(0, "");
+//         return 0;
+//     } 
+// }
 
 // [BUILD 4] Read CAN messages (steering angle)
 // CANStrategy canStrategy;
