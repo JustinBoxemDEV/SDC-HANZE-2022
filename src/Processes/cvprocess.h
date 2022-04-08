c3739a6f473ed6543a776b63480594d6e9898768
RobinvanWijk <robinvanwijk@home.nl>
2022-04-08 11:35:34 +0200

    linux fix

--- src/Processes/cvprocess.h
+++ src/Processes/cvprocess.h
@@ -6,7 +6,7 @@
 #include "../VehicleControl/communicationstrategy.h"
 #include "../ComputorVision/computorvision.h"
 #include "../PID/PID.h"
-#include "../MediaSources/streamsource.h"
+#include "../MediaSources/streamSource.h"
 #include <filesystem>
 
 namespace fs = std::filesystem;
