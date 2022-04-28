3bb9fe098fdcde0c4d858d75dce79022c09bdc3e
Sabine0 <sabineschreuderx@hotmail.com>
2022-04-28 16:43:51 +0200

    Agent examples

---
+++ src/MachineLearning/Other/CarRacingExamples/rl_carracing_example.py
@@ -0,0 +1,60 @@
+# !needs swig, python 3.7 and gym 0.21
+
+# conda install -c conda swig
+# conda create -n p37 python=3.7
+# pip install gym[box2d]=0.21
+
+import gym
+from stable_baselines3 import PPO
+from stable_baselines3.common.vec_env import DummyVecEnv
+from stable_baselines3.common.evaluation import evaluate_policy
+import os
+
+environment_name = "CarRacing-v0"
+env = gym.make(environment_name)
+env = DummyVecEnv([lambda: env]) # for making several instances
+
+# Load model
+log_path = os.path.join("src/MachineLearning/Other/", "training", "logs")
+ppo_path = os.path.join("src/MachineLearning/Other/", "training", "models", "PPO_Driving_Model")
+model = PPO.load(ppo_path, env=env)
+print(env.action_space)
+
+episodes = 5
+for episode in range(episodes):
+    observation = env.reset()
+    done = False
+    score = 0
+
+    while not done:
+        env.render()
+        action = model.predict(observation)
+        # print(f"action: {action}")
+        observation, reward, done, _ = env.step(action[0])
+        score += reward
+    print("Episode: {} Score: {}".format(episode, score))
+env.close()
+
+# --------------------------------------------------------------------------------------------------
+# In cmd run: tensorboard --logdir=. in the training/logs/PPO_1 folder to view the logs
+# For training a PPO model in carracing
+
+# log_path = os.path.join("src/MachineLearning/Other/", "training", "logs")
+# ppo_path = os.path.join("src/MachineLearning/Other/", "training", "models", "PPO_Driving_Model")
+
+# environment_name = "CarRacing-v0"
+# env = gym.make(environment_name)
+# env = DummyVecEnv([lambda: env])
+# env.render()
+
+# model = PPO("CnnPolicy", env, verbose=1, tensorboard_log=log_path, device="cuda")
+
+# # Load model (comment line 50 if you use this)
+# # model = PPO.load(ppo_path, env=env)
+
+# # Train model
+# model.learn(total_timesteps=50000)
+# model.save(ppo_path)
+
+
+# print(evaluate_policy(model, env, n_eval_episodes=10, render=True))
---
+++ src/MachineLearning/Other/CartPoleExamples/rl_cartpole_example.py
@@ -0,0 +1,55 @@
+# Training a PPO model in cartpole using stable baselines 3
+# https://stable-baselines3.readthedocs.io/en/master/
+
+import os
+import gym
+from stable_baselines3 import PPO
+from stable_baselines3.common.vec_env import DummyVecEnv
+from stable_baselines3.common.evaluation import evaluate_policy
+
+environment_name = "CartPole-v0"
+env = gym.make(environment_name)
+env = DummyVecEnv([lambda: env])
+
+# Load model
+PPO_path = os.path.join("src/MachineLearning/Other/", "training", "models", "PPO_Model_Cartpole")
+model = PPO.load(PPO_path, env=env)
+
+episodes = 5
+for episode in range(episodes):
+    obversation = env.reset()
+    ep_reward = 0
+    done = False
+    
+    while not done:
+        env.render()
+        action, _ = model.predict(obversation)
+        next_obversation, reward, done, _ = env.step(action)
+        
+        ep_reward += reward
+        obversation = next_obversation
+
+    print("Episode: {}, Episode reward: {}".format(episode, ep_reward))
+env.close()
+
+
+# --------------------------------------------------------------------------------------------------
+# In cmd run: tensorboard --logdir=. in the training/logs/PPO_1 folder to view the logs
+
+# For training a PPO model in cartpole
+# log_path = os.path.join("src/MachineLearning/Other/", "training", "logs")
+# PPO_path = os.path.join("src/MachineLearning/Other/", "training", "models", "PPO_Model_Cartpole")
+
+# environment_name = "CartPole-v0"
+# env = gym.make(environment_name)
+# env = DummyVecEnv([lambda: env])
+# model = PPO('MlpPolicy', env, verbose=1, tensorboard_log=log_path)
+
+# # Load model (comment line 46 if you use this)
+# # model = PPO.load(PPO_path, env=env)
+
+# # Train model
+# model.learn(total_timesteps=200000)
+# model.save(PPO_path)
+
+# print(evaluate_policy(model, env, n_eval_episodes=10, render=True))
