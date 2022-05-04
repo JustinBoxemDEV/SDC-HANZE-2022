81f179eb0b8c6e8227329a29e7f79f42b970fa09
Douwe0303 <d.de.leeuw@st.hanze.nl>
2022-05-04 16:58:21 +0200

    für Sabin

--- src/MachineLearning/ACRacing/testfile.py
+++ src/MachineLearning/ACRacing/testfile.py
@@ -21,9 +21,17 @@
 
 # ------------------------------------------------------------------------------------
 # Test model
+# import torch
+# import os
 # from AssettoCorsaEnv import AssettoCorsaEnv
+# from stable_baselines3 import PPO
+
+# save_path = os.path.join("src/MachineLearning/ACRacing/", "training", "models", "AC_model_3")
+
 # torch.cuda.empty_cache()
 # env = AssettoCorsaEnv()
+# model = PPO.load(save_path, env=env, device='cuda')
+
 # print('testing...')
 # episodes = 5
 # for episode in range(episodes):
@@ -43,58 +51,9 @@
 
 # ------------------------------------------------------------------------------------
 # count green pixels in region of interest (roi)
-# import cv2
-# from cv2 import imread
-# img_name_road = "C:/Users/Sabin/Documents/SDC/Screenshot 2022-04-29 193118.png"
-# img_name_gras = "C:/Users/Sabin/Documents/SDC/Screenshot 2022-04-29 200655.png"
-# img_name_little_grass = "C:/Users/Sabin/Documents/SDC/Screenshot 2022-04-29 210018.png"
-# img_name__very_little_grass = "C:/Users/Sabin/Documents/SDC/Screenshot 2022-04-29 210226.png"
-
-# img_name_720p = "C:/Users/Sabin/Documents/SDC/Screenshot 2022-04-29 215205_720p.png"
-
-# img_road = imread(img_name_road)
-# img_grass = imread(img_name_gras)
-# img_little_grass = imread(img_name_little_grass)
-# img_very_little_grass = imread(img_name__very_little_grass)
-
-# img_720p = imread("C:/Users/Sabin/Documents/SDC/Screenshot 2022-04-29 215205_720p.png")
-
-# # these values are on 1440p you will need to change them if you use this
-# roi_road = img_road[1250:1280, 870:1709]
-# roi_grass = img_grass[1250:1280, 870:1709]
-# roi_little_grass = img_little_grass[1250:1280, 870:1709]
-# roi_very_little_grass = img_very_little_grass[1250:1280, 870:1709]
-
-# roi_720p = img_720p[576:606, 315:1090]
-
-# cv2.imshow("roi road", roi_road)
-# cv2.imshow("roi grass", roi_grass)
-# cv2.imshow("roi little grass", roi_little_grass)
-# cv2.imshow("roi very little grass", roi_very_little_grass)
-# cv2.imshow("roi 720p", roi_720p)
-
-# # USES BGR INSTEAD OF RGB
-# # count_grass_pixels1 = 0
-# # for row in roi_road:
-# #     for pixel in row:
-# #         # print(pixel)
-# #         if pixel[1]-10 > pixel[0] and pixel[1]-10 > pixel[2]:
-# #             count_grass_pixels1 = count_grass_pixels1 + 1
-# # print("Grass pixels in image with only road:", count_grass_pixels1)
-
-# # count_grass_pixels2 = 0
-# # for row in roi_very_little_grass:
-# #     for pixel in row:
-# #         # print(pixel)
-# #         if pixel[1]-10 > pixel[0] and pixel[1]-10 > pixel[2]:
-# #             count_grass_pixels2 = count_grass_pixels2 + 1
-
-# # print("Grass pixels in image with very little grass:", count_grass_pixels2)
-
-# cv2.waitKey(0)
-
-# -------------------------------------------------------------------------------------------------
-# import win32gui
+import cv2
+from cv2 import imread
+import win32gui
 from PIL import ImageGrab
 import numpy as np
 import cv2
@@ -102,14 +61,46 @@ import cv2
 # ACWindow = win32gui.FindWindow(None, "Assetto Corsa")
 # rect = win32gui.GetWindowPlacement(ACWindow)[-1]
 # frame = np.array(ImageGrab.grab(rect))[:,:,::-1]
-# frame = frame[:720, :1280]
 
-frame = cv2.imread("/home/sab/Documents/Projects/SDC-HANZE-2022/src/MachineLearning/ACRacing/TestImges/ac480p.png") # 480p image
-# frame = frame[:100, :100]
+frame = cv2.imread("src/MachineLearning/ACRacing/TestImges/ac480p.pn") # 480p image for testing
+# frame = frame[30:510, 10:650] # 480p
+# cv2.imshow("img", frame)
 
-cv2.imshow("a", frame)
+hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-roi = frame[410:430, 180:585]
+cv2.imshow("hsv", hsv)
 
+hsv = cv2.medianBlur(hsv, 15)
+
+cv2.imshow("blur", hsv)
+
+# range green
+lower_green = np.array([32, 0, 0])
+print(lower_green)
+upper_green= np.array([55, 255, 255])
+mask = cv2.inRange(hsv, lower_green, upper_green)
+
+cv2.imshow("mask", mask)
+
+roi = mask[400:420, 130:530]
 cv2.imshow("roi", roi)
+
+green_pixels = 0
+for row in roi:
+    for pixel in row:
+        if pixel == 255:
+            green_pixels = green_pixels + 1
+            # print("woo")
+
+print("Green pixels:", green_pixels)
+
+# USES BGR INSTEAD OF RGB
+# count_grass_pixels1 = 0
+# for row in roi_road:
+#     for pixel in row:
+#         # print(pixel)
+#         if pixel[1]-10 > pixel[0] and pixel[1]-10 > pixel[2]:
+#             count_grass_pixels1 = count_grass_pixels1 + 1
+# print("Grass pixels in image with only road:", count_grass_pixels1)
+
 cv2.waitKey(0)
