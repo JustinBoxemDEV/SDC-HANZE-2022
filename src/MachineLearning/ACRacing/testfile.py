f84a86bf4fafae9dc047e2bbdec3302519d56edf
douwe <douwe0303@gmail.com>
2022-05-06 11:22:18 +0200

    Changed color range and added line detection

--- src/MachineLearning/ACRacing/testfile.py
+++ src/MachineLearning/ACRacing/testfile.py
@@ -4,8 +4,8 @@
 # count green pixels in region of interest (roi)
 import cv2
 from cv2 import imread
-import win32gui
-from PIL import ImageGrab
+# import win32gui
+#from PIL import ImageGrab
 import numpy as np
 import cv2
 
@@ -30,8 +30,8 @@ import cv2
 
 def setHSV(frame, blur):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    if(blur):
-        hsv = cv2.medianBlur(hsv, 27)
+    if(blur != 0):
+        hsv = cv2.medianBlur(hsv, blur)
     hsv = np.array(hsv)
     return hsv
 
@@ -46,84 +46,46 @@ def setColor(frame, mask, color):
     frame[mask>0]=color
     return frame
 
-frame = cv2.imread("TestImges/ac480p.png")# 480p image for testing
-# frame = cv2.imread("D:\\Github Desktop Clones\\SDC-HANZE-2022\\assets\\images\\assen.png") # 480p image for testing
+# frame = cv2.imread("TestImges/ac480pgrass.png")# 480p image for testing
+frame = cv2.imread("/home/douwe/Projects/SDC-HANZE-2022/assets/images/assen.png") # 480p image for testing
 frame = frame[30:510, 10:650] # 480p
+
+cv2.imshow("Original frame", frame)
 
 # assetto to IRL conversion
 
-hsv = setHSV(frame, True)
-mask = setMask(hsv, [18, 90, 40], [41, 145, 70]) # set mask for Assetto grass
+tweak = frame.copy();
+
+hsv = setHSV(frame, 0)
+mask = setMask(hsv, [0,43, 97], [28, 68, 159])
+frame = setColor(frame, mask, (255, 0, 0))
+
+hsv = setHSV(frame, 0)
+mask = setMask(hsv, [24, 74, 0], [70, 255, 255]) # set mask for Assetto grass
 frame = setColor(frame, mask, (35, 120, 100)) # set assetto grass to real life grass color
 
-hsv = setHSV(frame, True)
+hsv = setHSV(frame, 0)
 mask = setMask(hsv, [0, 0, 0], [25, 100, 150]) # set mask for Assetto road
 frame = setColor(frame, mask, (100, 81, 82)) # set assetto road to real life road color
 
+# tweak = frame.copy()
 cv2.imshow("Assetto TO IRL", frame)
 
 # set False if converting assetto to IRL, set True if using IRL image
-hsv = setHSV(frame, False)
+hsv = setHSV(frame, 0)
 
 # final grass mask
-mask = setMask(hsv, [23, 0, 0], [42, 255, 191]) # IRL grass mask
+# mask = setMask(hsv, [23, 0, 0], [42, 255, 191]) # IRL grass mask
 
 # final road mask
-# mask = setMask(hsv, [95, 0, 50], [155, 80, 150]) # IRL road mask
+mask = setMask(hsv, [98, 0, 0], [179, 82, 150]) # IRL road mask
 
 cv2.imshow("Final Mask", mask)
 
-# ACWindow = win32gui.FindWindow(None, "Assetto Corsa")
-# rect = win32gui.GetWindowPlacement(ACWindow)[-1]
-# frame = np.array(ImageGrab.grab(rect))[:,:,::-1]
+mask = cv2.medianBlur(mask, 3)
 
-roi = mask[400:420, 130:530]
-cv2.imshow("roi", roi)
+cv2.imshow("Final mask with blur", mask)
 
-green_pixels = 0
-for row in roi:
-    for pixel in row:
-        if pixel == 255:
-            green_pixels = green_pixels + 1
-            # print("woo")
-
-print("Green pixels:", green_pixels)
-
-# USES BGR INSTEAD OF RGB
-# count_grass_pixels1 = 0
-# for row in roi_road:
-#     for pixel in row:
-#         # print(pixel)
-#         if pixel[1]-10 > pixel[0] and pixel[1]-10 > pixel[2]:
-#             count_grass_pixels1 = count_grass_pixels1 + 1
-# print("Grass pixels in image with only road:", count_grass_pixels1)
-
-frame = cv2.imread("TestImges/ac480p.png")# 480p image for testing
-# frame = cv2.imread("D:\\Github Desktop Clones\\SDC-HANZE-2022\\assets\\images\\assen.png") # 480p image for testing
-frame = frame[30:510, 10:650] # 480p
-
-# assetto to IRL conversion
-
-hsv = setHSV(frame, True)
-mask = setMask(hsv, [18, 90, 40], [41, 145, 70]) # set mask for Assetto grass
-frame = setColor(frame, mask, (35, 120, 100)) # set assetto grass to real life grass color
-
-hsv = setHSV(frame, True)
-mask = setMask(hsv, [0, 0, 0], [25, 100, 150]) # set mask for Assetto road
-frame = setColor(frame, mask, (100, 81, 82)) # set assetto road to real life road color
-
-cv2.imshow("Assetto TO IRL", frame)
-
-# set False if converting assetto to IRL, set True if using IRL image
-hsv = setHSV(frame, False)
-
-# final grass mask
-mask = setMask(hsv, [23, 0, 0], [42, 255, 191]) # IRL grass mask
-
-# final road mask
-# mask = setMask(hsv, [95, 0, 50], [155, 80, 150]) # IRL road mask
-
-cv2.imshow("Final Mask", mask)
 
 # ACWindow = win32gui.FindWindow(None, "Assetto Corsa")
 # rect = win32gui.GetWindowPlacement(ACWindow)[-1]
@@ -144,8 +106,9 @@ print("Green pixels:", green_pixels)
 def nothing(x):
     pass
 
-tweak = frame.copy()
-tweak = cv2.medianBlur(tweak, 27)
+# tweak = cv2.imread("TestImges/ac480p.png")
+# tweak = frame.copy()
+# tweak = cv2.medianBlur(tweak, 27)
 
 # Create a window
 cv2.namedWindow('image')
