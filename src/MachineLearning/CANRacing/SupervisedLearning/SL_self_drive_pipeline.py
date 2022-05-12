d700f7e2a267eb470212b43fed3b71c09e1a6b40
Sabine0 <sabineschreuderx@hotmail.com>
2022-05-12 19:34:57 +0200

    Added AMP and TQDM

--- src/MachineLearning/CANRacing/SupervisedLearning/SL_self_drive_pipeline.py
+++ src/MachineLearning/CANRacing/SupervisedLearning/SL_self_drive_pipeline.py
@@ -2,14 +2,14 @@ import torch
 import skimage.io
 from matplotlib import pyplot as plt
 from load_data import get_dataloader
-import tqdm
+from tqdm import tqdm
 import numpy as np
 from SelfDriveModel import SelfDriveModel
 from utilities import static_var
 
-@torch.no_grad()
+
 def run_training(train_img_dir: str, train_actions_csv: str, valid_img_dir: str, valid_actions_csv: str, 
-                num_epochs: int = 5, batch_size: int = 8, dev: str = "cuda:0"):
+                num_epochs: int = 5, batch_size: int = 8, amp_on = False, dev: str = "cuda:0"):
                 
     train_loader = get_dataloader(img_folder=train_img_dir, act_csv=train_actions_csv, batch_size=batch_size, normalize=True) # set transforms to true here for data augmentation
     valid_loader = get_dataloader(img_folder=valid_img_dir, act_csv=valid_actions_csv, batch_size=batch_size, normalize=True)
@@ -18,39 +18,60 @@ def run_training(train_img_dir: str, train_actions_csv: str, valid_img_dir: str,
     model.to(dev)
     model.train()
 
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-    # TODO: maybe scheduler = optimizer
+    if amp_on:
+        scaler = torch.cuda.amp.GradScaler()
 
-    total_loss = 0
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=15, factor=0.1)
+
+    loss_fn = torch.nn.MSELoss()
 
     print("Training...")
     for epoch in range(0, num_epochs):
-        print(f"Executing epoch {epoch}!")
+        loss_sum, loss_cnt  = 0, 0
+        # print(f"Executing epoch {epoch}!")  
+        
 
-        for idx, batch in enumerate(train_loader):
-            print(f"Executing batch number {idx}!")
-
-            # print("Images in batch:", batch['image'])
-            # print("Ground truth actions in batch:", batch['actions'])
+        for idx, batch in enumerate(tqdm(train_loader)):
+            optimizer.zero_grad()
+            # print(f"Executing batch number {idx}!")
 
             input_images, actions = batch['image'].to(dev), batch['actions'].to(dev)
+            
+            # print("Images in batch:", batch['image'])
+            # print("Ground truth actions in batch:", batch['actions'])
+        
+            if amp_on:
+                # THIS IS EXPERIMENTAL, SET IT TO FALSE DURING TRAINING FOR NOW
+                with torch.cuda.amp.autocast():
+                    outputs = model(input_images)
+                    loss = loss_fn(outputs, actions)
 
-            optimizer.zero_grad()
+                scaler.scale(outputs=loss).backward()
+                torch.nn.utils.clip_grad_norm_(model.parameters(), 0.1)
+                scaler.step(optimizer=optimizer)
+                scaler.update()
+            else:
+                outputs = model(input_images)
+                # print("Prediction:", outputs)
+                # print("Ground truth:", actions)
 
-            outputs = model(input_images)
-            # print("Prediction:", outputs)
-            # print("Ground truth:", actions)
+                loss = loss_fn(outputs, actions)
+                loss.backward()
+                torch.nn.utils.clip_grad_norm_(model.parameters(), 0.1)
+                optimizer.step()
 
-            loss_fn = torch.nn.MSELoss()
-            loss = loss_fn(outputs, actions)
-            # TODO: maybe backward? (loss.backward())
+            loss_sum += loss.item()
+            loss_cnt += 1
 
-            optimizer.step()
+        avg_loss = loss_sum / loss_cnt
 
-            total_loss += loss.item()
+        # Tune learning rate
+        scheduler.step(avg_loss, epoch)
 
-        # if epoch % 2 == 0:
-        if True:
+        print(f"Avg loss on epoch {epoch} is {avg_loss}")
+
+        if epoch % 2 == 0:
             model.eval()
             run_validation(valid_loader=valid_loader, epoch=epoch, model=model, dev=dev)
             model.train()
@@ -61,7 +82,7 @@ def run_training(train_img_dir: str, train_actions_csv: str, valid_img_dir: str,
 @torch.no_grad()
 def run_validation(valid_loader, model, epoch, dev):
     loss_sum, loss_cnt = 0, 0
-    for _, batch in enumerate(valid_loader):
+    for _, batch in enumerate(tqdm(valid_loader)):
 
         input_images, actions = batch['image'].to(dev), batch['actions'].to(dev)
 
@@ -80,8 +101,8 @@ def run_validation(valid_loader, model, epoch, dev):
     if avg_loss < run_validation.best_loss:
         # save_dict = {"model": model.state_dict(), "loss": avg_loss, "epoch": epoch, "type": str(type(model))}
         # torch.save(save_dict, "C:/Users/Sabin/Documents/vsc_cpp_projects/SDC-stuff/SDC-HANZE-2022/src/MachineLearning/CANRacing/models/SelfDriveModel.pt") # Doesn't work for some reason
-        torch.save(model, "C:/Users/Sabin/Documents/vsc_cpp_projects/SDC-stuff/SDC-HANZE-2022/src/MachineLearning/CANRacing/models/SLSelfDriveModel.pt")
-        print(f"Saving model with loss: {avg_loss}")
+        torch.save(model, "C:/Users/Sabin/Documents/vsc_cpp_projects/SDC-stuff/SDC-HANZE-2022/src/MachineLearning/CANRacing/models/SLSelfDriveModelv2.pt")
+        print(f"Saving model at epoch {epoch} with loss {avg_loss}")
         run_validation.best_loss = avg_loss
     return
 
@@ -94,16 +115,14 @@ def run_testing(test_img_dir: str, test_actions_csv: str, dev="cuda:0"):
     # model.load_state_dict(torch.load("C:/Users/Sabin/Documents/vsc_cpp_projects/SDC-stuff/SDC-HANZE-2022/src/MachineLearning/CANRacing/models/SLSelfDriveModel.pt"))
     # print(f"Loaded model with loss: {model['loss']}")
 
-    model = torch.load("C:/Users/Sabin/Documents/vsc_cpp_projects/SDC-stuff/SDC-HANZE-2022/src/MachineLearning/CANRacing/models/SLSelfDriveModel.pt")
+    model = torch.load("C:/Users/Sabin/Documents/vsc_cpp_projects/SDC-stuff/SDC-HANZE-2022/src/MachineLearning/CANRacing/models/SLSelfDriveModelv2.pt")
     model.eval()
     model.to(dev)
 
     loss_sum, loss_cnt = 0, 0
 
     print("Testing...")
-    for idx, batch in enumerate(test_loader):
-        print(f"Executing batch number {idx}!")
-
+    for idx, batch in enumerate(tqdm(test_loader)):
         input_images, actions = batch['image'].to(dev), batch['actions'].to(dev)
 
         outputs = model(input_images)
@@ -111,6 +130,7 @@ def run_testing(test_img_dir: str, test_actions_csv: str, dev="cuda:0"):
         loss_fn = torch.nn.MSELoss()
         loss = loss_fn(outputs, actions)
 
+        # print(f"Batch loss: {loss}")
         loss_sum += loss.cpu().item()
         loss_cnt += 1
         
@@ -127,10 +147,10 @@ def run(training=False):
                     # TEST AND VALIDATION DATA IS CURRENTLY THE SAME DUE TO LACK OF DATA
                     valid_img_dir="C:/Users/Sabin/Documents/SDC/SL_data/validation/", 
                     valid_actions_csv="C:/Users/Sabin/Documents/SDC/SL_data/validation/douwe_data_images_18-11-2021 15-12-21.csv",
-                    num_epochs=1, batch_size=8, dev="cuda:0")
+                    num_epochs=2, amp_on=False, batch_size=3, dev="cuda:0")
 
         # try to free up GPU memory
-        # torch.cuda.empty_cache()
+        torch.cuda.empty_cache()
 
     run_testing(test_img_dir="C:/Users/Sabin/Documents/SDC/SL_data/testing/", 
                 test_actions_csv="C:/Users/Sabin/Documents/SDC/SL_data/testing/douwe_data_images_18-11-2021 15-12-21.csv", 
@@ -140,4 +160,4 @@ def run(training=False):
 
 
 if __name__ == "__main__":
-    run(training=False)
+    run(training=True)
