a11e7c587d26243f791d634b9886d74c408c9fd3
douwe <douwe0303@gmail.com>
2022-05-13 14:03:42 +0200

    Added twentytwo and twentyone

--- src/MachineLearning/ACRacing/rdwfixcsv.py
+++ src/MachineLearning/ACRacing/rdwfixcsv.py
@@ -5,6 +5,9 @@ import cv2
 readf = open('/home/douwe/Desktop/data/data_images_18-11-2021_15-12-21.csv')
 writef = open('/home/douwe/Desktop/data/test.csv', 'w')
 
+twentytwo = False
+twentyone = True
+
 fields=('Steer', 'Throttle', 'Brake', 'Image')
 
 writer = csv.DictWriter(writef, fieldnames=fields, lineterminator='\n')
@@ -13,56 +16,101 @@ reader = csv.reader(readf)
 writer.writeheader()
 header = next(reader)
 
-for row in reader:
-    text = str(row)
-    text = text.replace("\"", "").replace("[", "").replace("]", "").replace("\'", "")
+if(twentyone):
+    for row in reader:
+        text = str(row)
+        text = text.replace("\"", "").replace("[", "").replace("]", "").replace("\'", "")
 
-    # print(text)
-    # text = text.replace(";", "")
+        # print(text)
+        # text = text.replace(";", "")
 
-    data = text.split("|")
+        data = text.split("|")
 
-    image = data[4].replace("\"", "").replace(".png", ".jpg")
-    steering = data[0].replace("\"", "").split(",")
-    steering.reverse()
-    steeringData = [int(x) for x in steering]
-    steeringData.reverse()
-    steeringBytes = bytes(steeringData)
+        image = data[4].replace("\"", "").replace(".png", ".jpg")
+        steering = data[0].replace("\"", "").split(",")
+        steering.reverse()
+        steeringData = [int(x) for x in steering]
+        steeringData.reverse()
+        steeringBytes = bytes(steeringData)
 
-    steerFloat = struct.unpack('f', steeringBytes[0:4])
-    steerFloat = str(steerFloat).replace("(", "").replace(")", "").replace(",", "")
-    steerFloat = float(steerFloat)
+        steerFloat = struct.unpack('f', steeringBytes[0:4])
+        steerFloat = str(steerFloat).replace("(", "").replace(")", "").replace(",", "")
+        steerFloat = float(steerFloat)
 
-    imageFloat = image.split("/")
-    # print(imageFloat)
-    imageName = imageFloat[1]
-    # print(imageName)
+        imageFloat = image.split("/")
+        # print(imageFloat)
+        imageName = imageFloat[1]
+        # print(imageName)
+        
+        imageSource = '/home/douwe/Desktop/data/'+imageFloat[0]+'/'+imageFloat[1]
+        
+        rdwimage = cv2.imread(imageSource)
+        
+        if((steerFloat > 0.1) or (steerFloat < -0.1)):  
+            imageDestination = '/home/douwe/Desktop/data/bochten/'+imageName
+            cv2.imwrite(imageDestination, rdwimage)
+        if(steerFloat == 0):
+            imageDestination = '/home/douwe/Desktop/data/rechte stukken/'+imageName
+            cv2.imwrite(imageDestination, rdwimage)
+
+        throttle = data[1].replace("\"", "").split(",")[0]
+        if(len(throttle) == 0):
+            throttle = 0
+        else:
+            throttle = int(throttle) 
+            throttle = throttle * 1.75438596491
+            throttle = int(round(throttle))
+        
+        brake = data[2].replace("\"", "").split(",")[0]
+        if(len(brake) == 0):
+            brake = 0
+        else:
+            brake = int(brake)
+        writer.writerow({'Steer': steerFloat, 'Throttle': throttle, 'Brake': brake, 'Image': image})
+
+if(twentytwo):
+    for row in reader:
+        text = str(row)
+        text = text.replace("\"", "").replace("[", "").replace("]", "").replace("\'", "")
+
+        # print(text)
+        # text = text.replace(";", "")
+
+        data = text.split("|")
+
+        image = data[3].replace("\"", "")
+        steering = float(data[0])
+
+        imageFloat = image.split("/")
+        # print(imageFloat)
+        imageName = imageFloat[1]
+        # print(imageName)
+        
+        imageSource = '/home/douwe/Desktop/data/'+imageFloat[0]+'/'+imageFloat[1]
+        
+        rdwimage = cv2.imread(imageSource)
+        
+        if((steerFloat > 0.1) or (steerFloat < -0.1)):  
+            imageDestination = '/home/douwe/Desktop/data/bochten/'+imageName
+            cv2.imwrite(imageDestination, rdwimage)
+        if(steerFloat == 0):
+            imageDestination = '/home/douwe/Desktop/data/rechte stukken/'+imageName
+            cv2.imwrite(imageDestination, rdwimage)
+
+        throttle = data[1].replace("\"", "").split(",")[0]
+        if(len(throttle) == 0):
+            throttle = 0
+        else:
+            throttle = float(throttle) 
+            throttle = int(throttle * 100)
+            throttle = int(round(throttle))
+        
+        brake = float(data[2])
+        print("brake: ")
+        print(brake)
+        brake = int(brake * 100)
     
-    imageSource = '/home/douwe/Desktop/data/'+imageFloat[0]+'/'+imageFloat[1]
-    
-    rdwimage = cv2.imread(imageSource)
-    
-    if((steerFloat > 0.1) or (steerFloat < -0.1)):  
-        imageDestination = '/home/douwe/Desktop/data/bochten/'+imageName
-        cv2.imwrite(imageDestination, rdwimage)
-    if(steerFloat == 0):
-        imageDestination = '/home/douwe/Desktop/data/rechte stukken/'+imageName
-        cv2.imwrite(imageDestination, rdwimage)
-
-    throttle = data[1].replace("\"", "").split(",")[0]
-    if(len(throttle) == 0):
-        throttle = 0
-    else:
-       throttle = int(throttle) 
-       throttle = throttle * 1.75438596491
-       throttle = int(round(throttle))
-    
-    brake = data[2].replace("\"", "").split(",")[0]
-    if(len(brake) == 0):
-        brake = 0
-    else:
-        brake = int(brake)
-    writer.writerow({'Steer': steerFloat, 'Throttle': throttle, 'Brake': brake, 'Image': image})
+        writer.writerow({'Steer': steering, 'Throttle': throttle, 'Brake': brake, 'Image': image})
 
 writef.close()
 readf.close()
