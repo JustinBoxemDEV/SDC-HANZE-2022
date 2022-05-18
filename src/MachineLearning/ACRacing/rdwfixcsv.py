d45bda8477e063117aca93f3b31b68bb699e9f2b
Douwe0303 <d.de.leeuw@st.hanze.nl>
2022-05-18 10:23:54 +0200

    Fixed

--- src/MachineLearning/ACRacing/rdwfixcsv.py
+++ src/MachineLearning/ACRacing/rdwfixcsv.py
@@ -1,7 +1,11 @@
 import csv
 import struct
+import cv2
+import os
 
-readf = open('C:\\Users\\douwe\\Desktop\\data images 18-11-2021 11-07-14.csv')
+from sympy import Q
+
+readf = open("D:\\SL_data\\training\\data images 18-11-2021 15-12-21.csv")
 writef = open('C:\\Users\\douwe\Desktop\\test.csv', 'w')
 
 fields=('Steer', 'Throttle', 'Brake', 'Image')
@@ -13,11 +17,8 @@ writer.writeheader()
 header = next(reader)
 
 for row in reader:
-    print(row)
-
     text = str(row)
     text = text.replace("\"", "").replace("[", "").replace("]", "").replace("\'", "")
-    print(text)
 
     # print(text)
     # text = text.replace(";", "")
@@ -25,17 +26,24 @@ for row in reader:
     data = text.split("|")
 
     image = data[4].replace("\"", "")
+    # print(image)
+
     steering = data[0].replace("\"", "").split(",")
     steering.reverse()
     steeringData = [int(x) for x in steering]
     steeringData.reverse()
     steeringBytes = bytes(steeringData)
 
-    print(steeringBytes)
-
     steerFloat = struct.unpack('f', steeringBytes[0:4])
     steerFloat = str(steerFloat).replace("(", "").replace(")", "").replace(",", "")
     steerFloat = float(steerFloat)
+
+    if steerFloat == 0.0:
+        imageName = image.replace(".png", ".jpg").split("/")
+        turnImage = cv2.imread("D:\\SL_data\\training\\"+imageName[0]+"\\"+imageName[1])
+        turnImage = cv2.resize(turnImage, (640, 480))
+        path = "D:\\SL_data\\training\\recht stukken 18-11-2021 15-12-21"
+        cv2.imwrite(os.path.join(path, imageName[1]), turnImage)
 
     throttle = data[1].replace("\"", "").split(",")[0]
     if(len(throttle) == 0):
@@ -44,7 +52,6 @@ for row in reader:
        throttle = int(throttle) 
        throttle = throttle * 1.75438596491
        throttle = int(round(throttle))
-       print(throttle)
     
     brake = data[2].replace("\"", "").split(",")[0]
     if(len(brake) == 0):
