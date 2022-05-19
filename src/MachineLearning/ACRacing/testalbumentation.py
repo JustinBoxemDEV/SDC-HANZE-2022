c83c45d212f06a3922744521cf9cd0065f4a29fa
douwe <douwe0303@gmail.com>
2022-05-19 15:11:43 +0200

    Changed augmenation

--- src/MachineLearning/ACRacing/testalbumentation.py
+++ src/MachineLearning/ACRacing/testalbumentation.py
@@ -3,17 +3,17 @@ import cv2
 
 transform = A.Compose([
     # A.RandomSunFlare(flare_roi=(0, 0, 1, 0.25), angle_lower=0, angle_upper=1, num_flare_circles_lower=19, num_flare_circles_upper=20, src_radius=500),
-    A.HorizontalFlip(p=1),
-    # A.MotionBlur(blur_limit=10, p=1),
-    # A.RandomShadow(shadow_roi=(0, 0.55, 1, 1), num_shadows_lower=0, num_shadows_upper=3, p=0.6, shadow_dimension=85),
-    # A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, brightness_by_max=True, p=0.4),
-    # A.RandomGamma(gamma_limit=(120, 120), p=0.4)
+    # A.HorizontalFlip(p=1),
+    
+    A.MotionBlur(blur_limit=8, p=1),
+    # A.RandomBrightnessContrast(brightness_limit=0.1, contrast_limit=0.2, brightness_by_max=True, p=1),
+    # A.RandomGamma(gamma_limit=(60, 140), p=1)
 ])
 
-image = cv2.imread("D:\\Github Desktop Clones\\SDC-HANZE-2022\\src\\MachineLearning\\ACRacing\\TestImges\\1.png")
+image = cv2.imread("/home/douwe/Projects/SDC-HANZE-2022/src/MachineLearning/ACRacing/TestImges/real life/blurrySabin.jpg")
 # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-for i in range(10):
+for i in range(20):
 
     transformed = transform(image=image)
     transformed_imaged = transformed["image"]
