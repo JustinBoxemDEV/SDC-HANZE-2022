3757b88687beb94216e8841825f9ae86b36e4654
Douwe0303 <d.de.leeuw@st.hanze.nl>
2022-05-20 11:56:53 +0200

    Added csv throttle change script

--- src/MachineLearning/CANRacing/SupervisedLearning/load_data.py
+++ src/MachineLearning/CANRacing/SupervisedLearning/load_data.py
@@ -27,16 +27,12 @@ def get_dataloader(img_folder: str, act_csv: str, batch_size: int, normalize=Fal
     # Add transforms here!
     if normalize:
         t.append(Normalizer(0, 255))
-    if(random_sun_flare):
-        albu_t.append(A.RandomSunFlare(flare_roi=(0, 0, 1, 0.25), angle_lower=0, angle_upper=1,num_flare_circles_lower=1, num_flare_circles_upper=5, src_radius=350, p=0.5))
-    if(horizontal_flip):
-        albu_t.append(A.HorizontalFlip(p=0.5))
     if(motion_blur):
-        albu_t.append(A.MotionBlur(blur_limit=10, p=0.5))
-    if(random_shadow):
-        albu_t.append(A.RandomShadow(shadow_roi=(0, 0.55, 1, 1), num_shadows_lower=0, num_shadows_upper=3, shadow_dimension=85, p=0.5))
+        albu_t.append(A.MotionBlur(blur_limit=8, p=0.5))
+    # if(random_shadow):
+    #     albu_t.append(A.RandomShadow(shadow_roi=(0, 0.55, 1, 1), num_shadows_lower=1, num_shadows_upper=1, shadow_dimension=5, p=0.5))
     if(random_brightness_contrast):
-        albu_t.append(A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, brightness_by_max=True, p=0.5))
+        albu_t.append(A.RandomBrightnessContrast(brightness_limit=0.1, contrast_limit=0.2, brightness_by_max=True, p=0.5))
     if(random_gamma):
         albu_t.append(A.RandomGamma(gamma_limit=(80, 120), p=0.5))
 
