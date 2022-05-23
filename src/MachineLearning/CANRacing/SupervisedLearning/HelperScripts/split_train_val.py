c0f6c2c07bb8b54203fdffa33e57e187cd247f26
Sabine0 <sabineschreuderx@hotmail.com>
2022-05-23 10:27:49 +0200

    Update

--- src/MachineLearning/CANRacing/SupervisedLearning/HelperScripts/split_train_val.py
+++ src/MachineLearning/CANRacing/SupervisedLearning/HelperScripts/split_train_val.py
@@ -1,9 +1,43 @@
 # https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html
 
-# TODO: write script that takes 10% of training and splits it to validation
-# import sklearn.model_selection
-# sklearn.model_selection.model_selection
+"""
+This script is used for splitting the dataset in training and testing.
 
+Warning: Do not run this script blindly, it moves images :)
+"""
+import os
+import shutil
+from sklearn.model_selection import train_test_split
+
+
+# insert folder path here
+images = os.listdir(f'')
+
+# split 10% (this method is meant for splitting train/test but we use it to extract 10%)
+train, test = train_test_split(images, test_size=0.10, random_state=42)
+
+print("train has a size of", len(train))
+print("test has a size of", len(test))
+
+# Move the images to 'training/testing' folder
+# (i am moving instead of copying to check if all images are processed,
+# make sure you have a backup dataset if you do this. Otherwise use shutil.copy)
+for img in train:
+    print(f'Moving {img} to training set')
+    # img
+    shutil.move(
+        f'',
+        f'')
+
+for img in test:
+    print(f'Moving {img} to testing set')
+    shutil.move(
+        f'',
+        f'')
+
+
+# -----------------------------------------------------------------------
+# shitty script
 import os
 import random
 import shutil
